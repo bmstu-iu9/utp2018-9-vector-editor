1ae02edc1b89d84e91c76122b2fcfd6bc384d3ce
SyrbuDmitry <40852366+SyrbuDmitry@users.noreply.github.com>
2018-07-20 22:48:30 +0300

    Обновлен инструмент ластик

--- js/eraser.js
+++ js/eraser.js
@@ -3,6 +3,7 @@
 
 let eraser = document.getElementById('eraser');
 let isErasing = false;
+
 let fin1,len1,len2,end,strt;
 let minX = 1000, minY = 1000, newX = 0, newY,Path1,Path2,currentPath;
 drawPanel.addEventListener('mousedown',startErasing);
@@ -19,44 +20,6 @@ function dist(x1,y1,x2,y2){
   return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2))
 }
 
-function krug(path,i) {
-  console.log(i);
-  shape = document.createElementNS(svgNS, "circle");
-  shape.setAttribute('fill', 'red');
-  shape.setAttribute('cx', Number(path[i]));
-  shape.setAttribute('cy', Number(path[i + 1]));
-  shape.setAttribute('r', '10');
-  svgPanel.appendChild(shape);
-}
-function krug1(path) {
-  for (let i=0;i<path.length;i+=2) {
-    shape = document.createElementNS(svgNS, "circle");
-    shape.setAttribute('fill', 'blue');
-    shape.setAttribute('cx', Number(path[i]));
-    shape.setAttribute('cy', Number(path[i + 1]));
-    shape.setAttribute('r', '5');
-    svgPanel.appendChild(shape);
-  }
-}
-
-function parsePath(x, y, path) {
-  newX = -1;
-  end = 0;
-  strt = 0;
-  for (let i = 0; i + 2 < path.length;) {
-    c
-    if ((x > path[i] && x < path[i + 2]) && (y > (path[i + 1] - 10) && y < (path[i + 3] + 10))) {
-
-      newX = i;
-
-
-    }
-    i += 2;
-  }
-  position_one(path, x, y, newX);
-}
-
-
 
 function parseP(x, y, path) {
   newX = -1;
@@ -64,43 +27,39 @@ function parseP(x, y, path) {
   strt = 0;
 
   for (let i = 0; i + 2 < path.length;) {
-    if (((x > (path[i]) && x <= (path[i + 2])) || (x <= (path[i]) && x >= (path[i + 2]))) &&
-      ((y > (path[i + 1] - 10) && y < (path[i + 3] + 10)) || (y < (path[i + 1] - 10) && y > (path[i + 3] + 10)))) {
+    if (((x >= (path[i]) && x <= (path[i + 2])) || (x <= (path[i]) && x >= (path[i + 2]))) &&
+      ((y >= (path[i + 1]-5) && y <= (path[i + 3]+5)) || (y <= (path[i + 1]) && y >= (path[i + 3])))) {
       newX = i;
     }
     i += 2;
   }
-
-  if (newX!==-1)
-  {
+  console.log('newX: '+newX);
     e(path,x,y);
-  }
 }
 
 
 function e(path,x,y){
-  console.log(newX+' !!2!');
+
   let  l=newX,r=newX+2;
     while(dist(x,y,path[l],path[l+1])<5 && l>0)
     {
-      console.log('!@# '+dist(x,y,path[l],path[l+1]));
       l-=2;
     }
 
     while(dist(x,y,path[r],path[r+1])<5 && r<path.length-2)
     {
-      console.log('!@..# '+dist(path[newX],path[newX+1],path[r],path[r+1]));
+
       r+=2;
     }
   if (l<0)
     l=0;
   if (r>path.length-2)
     r=path.length-2;
-    console.log(l,r,newX);
     let k;
+    console.log(path);
     if(dist(path[0],path[1],path[path.length-2],path[path.length-1])<10)
     {
-      console.log('del ta');
+      console.log('delta');
       currentPath.remove();
       newX=-1;
       return;
@@ -108,43 +67,38 @@ function e(path,x,y){
     if(dist(x,y,path[0],path[1])<5)
     {
       strt=1;
-      console.log('strt');
     }
+    console.log('start '+strt+' '+dist(x,y,path[0],path[1]));
   if(dist(x,y,path[path.length-2],path[path.length-1])<5)
     {
-      console.log('end');
+
       end=1;
     }
 
 
-    k = (dist(x, path[l + 1], path[l], path[l + 1]) - 10) / dist(x, path[l + 1], path[l], path[l + 1]);
-    console.log('k1dadsada ' + k * x);
-    Path1 = 'M ' + path.slice(0, 2).join(' ') + ' L ' + path.slice(2, l + 2).join(' ') + ' ' + (path[l] + (x - path[l]) * k) + ' ' + path[l + 1];
-    k = (dist(x, path[l + 1], path[l], path[l + 1]) + 5) / dist(x, path[l + 1], path[l], path[l + 1]);
-    console.log('k2 ' + k);
-    Path2 = 'M ' + (x + 5) + ' ' + path[r + 1] + ' L ' + path.slice(r).join(' ');
-  
+
+      let dis=dist(x, y, path[l], path[l + 1]);
+      let dis1=dist(path[l], path[l+1], path[r], path[r + 1]);
+      if(dis<5){
+
+        dis=dist(x, y, path[r], path[r + 1]);
+
+        k = (dis-5)/ dis1;
+        Path2 = 'M ' +  (path[r] - (path[r]-path[l]) * k)+ ' ' + (path[r+1] - (path[r+1]-path[l+1]) * k) + ' L ' + path.slice(r).join(' ');
+
+        return;
+      }else {
+        k = (dis - 5) / dis1;
+        Path1 = 'M ' + path.slice(0, 2).join(' ') + ' L ' + path.slice(2, l + 2).join(' ') + ' ' + (path[l] + (path[r]-path[l]) * k) + ' ' + (path[l + 1] + (path[r + 1] - path[l + 1]) * k);
+        k = (dis + 5) / dis1;
+        Path2 = 'M ' + (path[l] + (path[r]-path[l]) * k) + ' ' + (path[l + 1] + (path[r + 1] - path[l + 1]) * k) + ' L ' + path.slice(r).join(' ');
+        return;
+      }
+
 }
 
 
-function position_one(path, x, y, newX) {
-  if (path.length === 4)
-    if (Math.abs(path[0] - path[2]) < 10 || Math.abs(path[1] - path[3]) <10) {
-      newX = -1;
-      currentPath.remove();
-      return;
-    }
-  if ((x - 5) < path[0])
-    strt = 1;
-  if ((x + 5) > path[path.length - 2])
-    end = 1;
-  if ((y - 5) < path[1])
-    strt = 1;
-  if ((y + 5) > path[path.length - 1])
-    end = 1;
-  Path1 = 'M ' + path.slice(0, 2).join(' ') + ' L ' + (x - 5) + ' ' + 247;
-  Path2 = 'M ' + (x + 5) + ' ' + 247 + ' L ' + path.slice(newX + 2).join(' ');
-}
+ 
 
 
 
@@ -154,6 +108,7 @@ strt=0;
 end=0;
   if (isErasing) {
     if (event.target.toString() === '[object SVGPathElement]') {
+      console.log('path');
       currentPath = event.target;
       if(c===0) {
         c++;
@@ -161,12 +116,12 @@ end=0;
       parseP(getMouseCoords(event).x, getMouseCoords(event).y, event.target.getAttribute('d').split(' ').map(Number).filter(Boolean));
       if(newX!==-1) {
         currentPath.remove();
-        if (Path1[Path1.length - 1] !== ' ' && strt!==1) {
+        if (strt!==1 && Path1[Path1.length - 1] !== ' ') {
           shape = document.createElementNS(svgNS, "path");
           shape.setAttribute('fill', 'none');
           shape.setAttribute('stroke-width', width);
           shape.setAttribute('stroke', 'black');
-          shape.setAttribute('stroke-linecap', 'round');
+          shape.setAttribute('stroke-linecap', 'butt');
           shape.setAttribute('stroke-linejoin', linejoin);
           shape.setAttribute('d', Path1);
           svgPanel.appendChild(shape);
@@ -176,7 +131,7 @@ end=0;
           shape.setAttribute('fill', 'none');
           shape.setAttribute('stroke-width', width);
           shape.setAttribute('stroke', 'black');
-          shape.setAttribute('stroke-linecap', 'round');
+          shape.setAttribute('stroke-linecap', 'butt');
           shape.setAttribute('stroke-linejoin', linejoin);
           shape.setAttribute('d', Path2);
           svgPanel.appendChild(shape);
