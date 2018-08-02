2908342b0a49a24a74d4571ac358fc48a65fdc2c
AlisherRogov <40853503+AlisherRogov@users.noreply.github.com>
2018-08-02 21:12:43 +0300

    Обновлен text.js
    Исправлен баг со  сдвигом свг панели.

--- js/text.js
+++ js/text.js
@@ -1,20 +1,41 @@
 'use strict'
 
 const text = document.getElementById("text");
-const drawPanelInner = drawPanel.innerHTML;
-let textEntered = false;
+let textForeignObject;
+let textDiv;
 
 function addText() {
+    textForeignObject = svgPanel.getElementById('textForeignObject');
+
     if(!text.checked) {
-        drawPanel.setAttribute('contenteditable', false);
-        if(textEntered) drawPanel.innerHTML += drawPanelInner;
+        textForeignObject.getElementsByTagName('div')[0].setAttribute('contenteditable', false);
         return;
     }
-    drawPanel.setAttribute('contenteditable', true);
-    if(drawPanel.innerHTML){
-         drawPanel.innerHTML = '';
-         textEntered = true;
-     }
+
+    if(textForeignObject) {
+        if(text.checked) {
+            textForeignObject.getElementsByTagName('div')[0].setAttribute('contenteditable', true);
+        }
+        return;
+    }
+
+    textForeignObject = document.createElementNS(svgNS, 'foreignObject');
+    textForeignObject.setAttribute('id', 'textForeignObject');
+    textForeignObject.setAttribute('height', 500);
+    textForeignObject.setAttribute('width', 900);
+
+    svgPanel.appendChild(textForeignObject);
+
+    textDiv = document.createElement('div');
+    textDiv.style.cssText = "overflow: auto; \
+    width: 900px; \
+    height: 500px; \
+    ";
+    textDiv.setAttribute('id', 'textDiv');
+    textDiv.setAttribute('contenteditable', true);
+    textDiv.textContent = 'Enter your text';
+
+    textForeignObject.appendChild(textDiv);
 }
 
 drawPanel.addEventListener('click', addText);
