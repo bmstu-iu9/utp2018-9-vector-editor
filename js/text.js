c44f0713bdf80a7962280bd22d624b72b16c3679
AlisherRogov <40853503+AlisherRogov@users.noreply.github.com>
2018-08-12 22:47:54 +0300

    Добавлена панель опций
    Исправлено множество недостатков, добавлена панель опций.

--- js/text.js
+++ js/text.js
@@ -1,22 +1,32 @@
 /*
-    Скрипт для создания области для пользовательского ввода текста. Создать область можно двумя способами:
-    1) Нажатием ЛКМ на холст, при этом обасть создается в точке нажатия и размерами по умолчание(336 : 200);
-    2) Нажатием ЛКМ и не отжимая провести диагональ четырехугольника нужного размера.
-    Также можно перемещать и менять размеры нашей области.
-    Как только пользователь создал область, текущий инструмент меняется на Курсор.
+    Скрипт для создания области для пользовательского ввода текста.
+    Нажатием ЛКМ на холсте можно создать только первую область, а для дальнейшего создания требуется сочетание Ctrl + ЛКМ,
+    при этом обасть создается в точке нажатия и с размерами по умолчанию(336 : 200)px;
+    Также можно перемещать и менять размеры данной области.
     Используется скрипт прямоугольника.
-    Также если текст выходит больше чем заданные границы области, то в область добавляется опция прокручивания.
+    Строка автоматически переходит на новую, если достигла границы области.
+    Если текст выходит больше чем заданные границы области, то в область добавляется опция прокручивания.
+    Также при помощи панели опций можно менять шрифт, размер шрифта, цвет.
+    Чтоб изменить, следует двумя нажатиями ЛКМ выбрать опцию, которую желаете применить.
+    По умолчанию шрифт: Arial, размер шрифта: 13pt, и цвет: черный.
+    Редактировать текст можно только при выборе курсора или текста.
+    Также имеется возможность использовать горячие сочетания клавиш:
+    Ctrl + B - полужирный (тег STRONG).
+    Ctrl + I	 - курсив (тег EM).
+    Ctrl + U	 - подчеркнутый (тег U).
+    Ctrl + Z - отмена последнего действия
 */
 'use strict';
 
 const text = document.getElementById('text');
 
-
 const createSVG_Text = (f = 'none', s = '#000000', sw = '3', so = '1', fo = '1') => {
     const textForeignObject = createSVGElem('foreignObject', f, s ,sw, so, fo);
     const textDiv = document.createElement('div');
     textDiv.setAttribute('contenteditable', true);
-    textDiv.style.cssText = "overflow: auto;";
+    textDiv.style.overflow = 'auto';
+    textDiv.style.overflowWrap = 'break-word';
+    textDiv.style.wordWrap = 'break-word';
     textDiv.textContent = 'Enter your text';
     textForeignObject.appendChild(textDiv);
     return textForeignObject;
@@ -30,70 +40,121 @@ class TextBox extends Figure {
     }
 
     static draw(event) {
-        if (!text.checked) {
+        if (!text.checked || (svgPanel.getElementsByTagName('foreignObject')[0] && !event.ctrlKey) ) {
             return;
         }
 
         let click = getMouseCoords(event);
-        const tbox = new TextBox(createSVG_Text());
-
+        const options = optionsText.getElementsByTagName('input');
+        const tbox = new TextBox(createSVG_Text('#FFFFFF', '#000000', '1', '1', '1'));
 
         svgPanel.appendChild(tbox.svgFig);
         ({ x: tbox.rect.x, y: tbox.rect.y } = click);
-        tbox.rect.moveByAngeles(click, {x:click.x + 336, y: click.y + 200 });
-        tbox.synchronizeWithRect();
-        cursor.checked = true;
-        currentInstrument = document.getElementById('cursor');
-        changeLabelSelected();
 
-        const moveTextBox = (e) => {
-            const current = getMouseCoords(e);
-            if (e.altKey) {
-                click = tbox.rect.getSymmetrical(current);
-            }
-            tbox.rect.moveByAngeles(click, current);
+        tbox.rect.height = 200;
+        tbox.rect.width = 336;
+        tbox.div.style.fontFamily = options[0].value;
+        tbox.div.style.fontSize = '13pt';
+
+        optionsText.getElementsByTagName('input')[1].value = '13';
+        tbox.rect.center.setCoords(tbox.rect.c);
+        tbox.synchronizeWithRect();
+        svgPanel.appendChild(tbox.rect.svgFig);
+
+        for (let i = 0; i < tbox.rect.refPoints.length; i++) {
+            tbox.rect.refPoints[i].figure = tbox;
+        }
+        tbox.rect.center.figure = tbox;
+
+        tbox.rect.updateRefPointsCoords();
+        tbox.hideOrShow();
+        tbox.showRefPoints();
+
+        ['click', 'mouseover', 'mouseout'].forEach(e => {
+            tbox.svgFig.addEventListener(e, () => tbox.rect.svgFig.dispatchEvent(new Event(e)));
+        });
+
+        const update = tbox.rect.updateRefPointsCoords.bind(tbox.rect);
+        tbox.rect.updateRefPointsCoords = () => {
+            update();
             tbox.synchronizeWithRect();
         };
 
-        const stopMoving = () => {
-            document.removeEventListener('mousemove', moveTextBox);
-            drawPanel.removeEventListener('mouseup', stopMoving);
-            svgPanel.appendChild(tbox.rect.svgFig);
+        tbox.rect.createTmpCopy = tbox.createTmpCopy.bind(tbox);
+        tbox.rect.deleteTmpCopy = tbox.deleteTmpCopy.bind(tbox);
+        tbox.finished = tbox.rect.finished = true;
 
-            tbox.rect.updateRefPointsCoords();
-            tbox.rect.hideOrShow(true, rect, () => svgPanel.removeChild(tbox.rect.svgFig),
-                                            () => svgPanel.appendChild(tbox.rect.svgFig));
-            tbox.rect.showRefPoints();
-
-            ['click', 'mouseover', 'mouseout'].forEach(e => {
-                tbox.svgFig.addEventListener(e, () => tbox.rect.svgFig.dispatchEvent(new Event(e)));
-            });
-
-            const update = tbox.rect.updateRefPointsCoords.bind(tbox.rect);
-            tbox.rect.updateRefPointsCoords = () => {
-                update();
-                tbox.synchronizeWithRect();
-            };
-
-            tbox.rect.createTmpCopy = tbox.createTmpCopy.bind(tbox);
-            tbox.rect.deleteTmpCopy = tbox.deleteTmpCopy.bind(tbox);
-        };
-
-        /*const changeToSvg = () => {
-            if(text.checked) {
-                return;
+        document.addEventListener('click', () =>  {
+            if(text.checked || cursor.checked) {
+                tbox.div.setAttribute('contenteditable', 'true');
+            } else {
+                if(svgPanel.compareDocumentPosition(tbox.rect.svgFig) & 16) {
+                    svgPanel.removeChild(tbox.rect.svgFig);
+                    tbox.rect.hideRefPoints();
+                }
+                tbox.div.setAttribute('contenteditable', 'false');
+                tbox.div.style.webkitTouchCallout = 'none';
+                tbox.div.style.webkitUserSelect = 'none';
+                tbox.div.style.khtmlUserSelect = 'none';
+                tbox.div.style.mozUserSelect = 'none';
+                tbox.div.style.msUsertSelect = 'none';
+                tbox.div.style.userSelect = 'none';
             }
-            const svgText = createSVGElem('text');
-            svgText.textContent = tbox.div.textContent;
-            svgText.setAttribute('x', tbox.x);
-            svgText.setAttribute('y', tbox.y);
-            svgPanel.insertBefore(svgText, this.svgFig);
-        }*/
+        });
+    }
 
-        document.addEventListener('mousemove', moveTextBox);
-        drawPanel.addEventListener('mouseup', stopMoving);
-        //drawPanel.addEventListener('click', changeToSvg);
+    hideOrShow(isShowing = true) {
+        currentFigure = this;
 
+        const check = ( () => {
+            return this.refPoints !== undefined && !this.somePointTaken && !someFigureTaken;
+        }).bind(this);
+
+        const hide = ( () => {
+            if (isShowing && this.refPoints !== undefined) {
+                this.hideRefPoints();
+                if (currentFigure == this) {
+                    if (cursor.checked) {
+                        hideAllOptions();
+                    }
+                    currentFigure = null;
+                }
+                isShowing = false;
+            }
+        } ).bind(this);
+        drawPanel.addEventListener('mousedown', hide);
+
+        this.svgFig.addEventListener('mousedown', ( () => {
+            if (check() && !isShowing && (text.checked || cursor.checked)) {
+                this.showRefPoints();
+                this.showOptions();
+                currentFigure = this;
+                isShowing = true;
+            }
+        } ).bind(this));
+
+        this.svgFig.addEventListener('mouseover', () => {
+            if (check()) {
+                drawPanel.removeEventListener('mousedown', hide);
+            }
+        });
+        this.svgFig.addEventListener('mouseout', () => {
+            if (check()) {
+                drawPanel.addEventListener('mousedown', hide);
+            }
+        });
+    }
+
+    showRefPoints() {
+        svgPanel.appendChild(this.rect.svgFig);
+        this.rect.showRefPoints();
+    }
+
+    hideRefPoints() {
+        if(svgPanel.compareDocumentPosition(this.rect.svgFig) & 16){
+            svgPanel.removeChild(this.rect.svgFig);
+            this.rect.hideRefPoints();
+        }
     }
 
     synchronizeWithRect() {
@@ -104,12 +165,19 @@ class TextBox extends Figure {
     createTmpCopy() {
         this.copy = createSVG_Text('none', '#000000', '1', '0.5', '0.5');
         this.copy.firstChild.style.opacity = '0.5';
-        this.copy.firstChild.textContent = this.div.textContent;
+        this.copy.firstChild.innerHTML = this.div.innerHTML;
         this.copy.setAttribute('x', this.x);
         this.copy.setAttribute('y', this.y);
         this.copy.setAttribute('width', this.width);
         this.copy.setAttribute('height', this.height);
         svgPanel.insertBefore(this.copy, this.svgFig);
+    }
+
+    showOptions() {
+        if(text.checked || cursor.checked) {
+            hideAllOptions();
+            optionsText.classList.add('show-option');
+        }
     }
 
     set x(v) {
@@ -136,3 +204,46 @@ class TextBox extends Figure {
 }
 
 drawPanel.addEventListener('mousedown', TextBox.draw = TextBox.draw.bind(TextBox));
+
+{
+    const inputs = optionsText.getElementsByTagName('input');
+    const selectors = optionsText.getElementsByTagName('ul');
+    const buttons = optionsText.getElementsByTagName('button');
+
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].setAttribute('readonly', '');
+    }
+    selectors[0].addEventListener("mousedown", (evt) => {
+      evt.preventDefault();
+      document.execCommand('fontname', false, inputs[0].value);
+    });
+
+    const changeFont = (fontSizeValue) => {
+        document.execCommand("fontSize", false, "7");
+        const fontElements = document.getElementsByTagName("font");
+        for (let i = 0, len = fontElements.length; i < len; i++) {
+            if (fontElements[i].size == "7") {
+                fontElements[i].removeAttribute("size");
+                fontElements[i].style.fontSize = fontSizeValue + 'pt';
+            }
+        }
+    }
+
+    selectors[1].addEventListener("mousedown", (evt) => {
+      evt.preventDefault();
+      changeFont(inputs[1].value);
+    });
+
+    selectors[2].addEventListener("mousedown", (evt) => {
+      evt.preventDefault();
+      let color = 'black';
+      if(inputs[2].value[0] == "К") {
+          color = 'red';
+      }else if(inputs[2].value[0] == "C") {
+          color = 'blue';
+      }else if(inputs[2].value[0] == "З") {
+          color = 'green';
+      }
+      document.execCommand('forecolor', false, color);
+    });
+}
